b887a2fe7649aa45cdde02069f76999d95992c1b
zevgaslin <zevgaslin@gmail.com>
2024-10-25 15:49:44 -0500

    dis shit

--- MyClassProject/src/myclassproject/mystorygraph/MyChoiceLabels.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyChoiceLabels.java
@@ -2,6 +2,6 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the EDGES of your story graph.
 public enum MyChoiceLabels {
-	Town, TownGeneralStore, GeneralStore, 
+	Town, TownGeneralStore, GeneralStore, Gate, City, Dock, Forest, Blacksmith, Alchemy, CastleGate
 }
 
