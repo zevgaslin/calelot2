5bad4b3335f386543cd7aeffe78da6774512730f
zevgaslin <zevgaslin@gmail.com>
2024-11-22 18:06:29 -0600

    override

--- MyClassProject/src/myclassproject/mystorygraph/MyNodeBuilder.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyNodeBuilder.java
@@ -46,9 +46,13 @@ public class MyNodeBuilder extends NodeBuilder {
 	public void TownActions() {
 		var node = get(MyNodeLabels.Town.toString());
 		node.add(new HideMenu()).add(new EnableInput());
+<<<<<<< Updated upstream
 		node.add(new NarrationSequence("The evil king Gorlock hath raised taxes upon thy farm, and you could not afford the new fees. As such, Gorlock sent his goons to roughen you up and repossess your family farm! You awaken in the small, poor town of Gregoria, on the outer edge of the forest. The town is filled with dilapidated buildings and broken people. You know what to do next. \n"
 				+ "Where would you like to go?"));
 		
+=======
+		//node.add(new NarrationSequence("You awaken in the small, poor town of Gregoria, on the outer edge of the forest. The town is filled with dilapidatedbuildings and broken people. You know what to do next. Where would you liketo go?"));
+>>>>>>> Stashed changes
 	}
 
 //Zev Gaslin
